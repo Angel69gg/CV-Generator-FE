02f0f552df1dd42af230d3199d4427270b0037f6
Roberto Friber <75671954+FriberNamzug@users.noreply.github.com>
2022-08-08 00:34:24 -0500

    feat: The services to consume were added

--- src/services/cv.js
+++ src/services/cv.js
@@ -1,70 +1,70 @@
 import axios from 'axios';
+import download from "downloadjs"
 
 export const addCV = async (data) => {
-    const data = {
+    const response = await axios.post('/api/add', {
         datosPerfil: {
-            nombre: data.nombre,
-            apellido: data.apellido,
-            edad: data.edad,
-            genero: data.genero,
-            descripcion: data.descripcionPerfil,
-            fechaNacimiento: data.fechaNacimiento,
+            nombre: data.datosPerfilnombre,
+            apellido: data.datosPerfilapellido,
+            edad: Number(data.datosPerfiledad),
+            genero: data.datosPerfilgenero,
+            descripcion: data.datosPerfildescripcion,
+            fechaNacimiento: data.datosPerfilfechaNacimiento
         },
-        contacto:{
-            email: data.email,
-            telefono: data.telefono,
-            linkedin: data.linkedin,
+        contacto: {
+            email: data.contactoemail,
+            telefono: data.contactotelefono,
+            linkedin: data.contactolinkedin,
         },
-        experiencia:{
-            empresa: data.empresa,
-            cargo: data.cargo,
-            fechaInicio: data.fechaInicioExperiencia,
-            fechaFin: data.fechaFinExperiencia,
-            descripcion: data.descripcionPerfil,
+        experiencia: {
+            empresa: data.experienciaempresa,
+            cargo: data.experienciacargo,
+            fechaInicio: data.experienciafechaInicio,
+            fechaFin: data.experienciafechaFin,
+            descripcion: data.experienciadescripcion,
         },
-        educacion:{
-            titulo: data.titulo,
-            institucion: data.institucion,
-            fechaInicio: data.fechaInicioEscolaridad,
-            fechaFin: data.fechaFinEscolaridad,
-            descripcion: data.descripcionescolaridad,
+        educacion: {
+            titulo: data.educaciontitulo,
+            institucion: data.educacioninstitucion,
+            fechaInicio: data.educacionfechaInicio,
+            fechaFin: data.educacionfechaFin,
+            descripcion: data.educaciondescripcion,
         },
-        idiomas:{
-            idioma: data.idioma,
-            nivel: data.nivelIdioma,
+        idiomas: {
+            idioma: "Sin informacion",
+            nivel: "Sin informacion",
         },
-        habilidades:{
-            habilidad: data.habilidad,
-            nivel: data.nivelHabilidad,
+        habilidades: {
+            habilidad: "Sin informacion",
+            nivel: "Sin informacion",
         },
-        fortalezas:{
-            fortaleza: data.fortaleza
+        fortalezas: {
+            fortaleza: "Sin informacion",
         },
-        debilidades:{
-            debilidad: data.debilidad
+        debilidades: {
+            debilidad: "Sin informacion",
         },
-        proyectos:{
-            proyecto: data.proyecto,
-            descripcion: data.descripcionProyecto,
-            fechaInicio: data.fechaInicioProyecto,
-            fechaFin: data.fechaFinProyecto,
-            url: data.urlProyecto,
+        proyectos: {
+            proyecto: "Sin informacion",
+            descripcion: data.proyectosdescripcion,
+            fechaInicio: data.proyectosfechaInicio,
+            fechaFin: data.proyectosfechaFin,
+            url: "Sin informacion",
         },
-        redes:{
-            red: data.red,
-            url: data.urlRed,
+        redes: {
+            red: data.redesnombre,
+            url: data.redesurl,
         },
-    }
-
-    const response = await axios.post('/api/add', data);
+    });
     return response;
 }
 
-export const addCVImg = async (data, id) => {
-    form = new FormData();
-    form.append('avatar', data.file);
+export const addCVImg = async (id, data) => {
+    const formData = new FormData();
+    formData.append('avatar', data);
 
-    const response = await axios.post(`/api/addImg/${id}`, form);
+
+    const response = await axios.post(`/api/addImg/${id}`, formData);
     return response;
 }
 
@@ -80,5 +80,17 @@ export const updateCV = async (data, id) => {
 
 export const deleteCV = async (id) => {
     const response = await axios.delete(`/api/delete/${id}`);
+    return response;
+}
+
+export const generarPDF = async (id) => {
+    const response = await axios.get(`/api/generar_pdf/${id}`);
+    //Obtenemos el archivo
+   if (response.status === 200) {
+    const pdf = `data:application/pdf;base64,${response.data.file}`;
+    download(pdf, `${id}.pdf`, "application/pdf");
+  } 
+
+
     return response;
 }
