78c6c7b8b46d4133c12db3cabeddcabd2bf51835
diana <dianlauracarranza@gmail.com>
2022-08-08 15:52:36 -0500

    docs: Translations are documented

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -26,14 +26,14 @@ export default function Login() {
             <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
                 <div className="mb-4">
                     <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="username">
-                    {t("login.usuario")}
+                    {t("login.usuario")} {/* esta linea nos genera la traduccion del texto */}
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" placeholder="Username">
                     </input>
                 </div>
                 <div className="mb-6">
                     <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="password">
-                    {t("login.contrasena")}
+                    {t("login.contrasena")} {/* esta linea nos traduce el texto contraseña */}
                     </label>
                     <input className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="******************">
                     </input>
@@ -41,7 +41,7 @@ export default function Login() {
                 </div>
                 <div className="flex items-center justify-between">
                     <Button variant="contained" color="success" type="submit" onClick={authenticate}>
-                    {t("login.iniciar")}
+                    {t("login.iniciar")} {/* este texto traduce el boton */}
                     </Button>
                 </div>
             </div>
